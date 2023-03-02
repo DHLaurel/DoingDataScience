1102d55b10be6d2f3518ce7f8e80a910222640b8
David Laurel <xxmz_ghettoxx@hotmail.com>
2023-03-01 20:05:59 -0600

    DS-6306 / Adding question #8 ; samples for question #9

--- BeersAndBreweries/CaseStudyQuestions.R
+++ BeersAndBreweries/CaseStudyQuestions.R
@@ -1,14 +1,19 @@
+library(tidyverse) # merge
 library(ggplot2)
 library(ggpmisc)
 library(ggthemes)
 library(mice) # multi-variate imputation
+library(class) # knn
+library(caret) # confusion matrix
 
 # 3.
 
 # breweries_file = file.choose()
 # beer_file = file.choose()
-brew_dat = read.csv(breweries_file)
+mj_file = file.choose()
+# brew_dat = read.csv(breweries_file)
 beer_dat = read.csv(beer_file)
+mj_dat = read.csv(mj_file)
 
 beer_brews = merge(x = brew_dat, y = beer_dat, by.x = "Brew_ID", by.y = "Brewery_id")
 colnames(beer_brews)[colnames(beer_brews) == "Name.x"] <- "Brew_Name"
@@ -81,7 +86,70 @@ ggplot(beer_brew_whole, aes(x = ABV, y = IBU)) +
   theme_wsj()   +
   theme(axis.title=element_text(size=12))
 
+# 8.
+pick_k <- function(trainData, testData, trainColumns, testColumn, kUpper){
+  cl <- trainData[,testColumn]
+  kTest = testData[,trainColumns]
+  k_frame <- data.frame(matrix(ncol = 2, nrow = 0))
+  colnames(k_frame) = c("KVal", "Accuracy")
+  for (k in 1:kUpper)
+  {
+    predicted <- knn(trainData[,trainColumns], kTest, cl, k)
+    num_incorrect <- sum(predicted != testData[,testColumn])
+    miss_rate <- num_incorrect / nrow(testData)
+    accuracy = 1 - miss_rate
+    k_frame[nrow(k_frame) + 1,] <- c(k, accuracy)
+    #print(paste('k: ', k, ', miss_rate: ', miss_rate))
+  }
+  [order(k_frame$Accuracy, decreasing = TRUE )[1],][[1]]
+  k = k_frame
+  k
+}
+bb_ales <- beer_brew_whole[grepl("Ale", beer_brew_whole$Style, ignore.case = TRUE) | grepl("IPA", beer_brew_whole$Style, ignore.case=TRUE), ]
+bb_ales <- bb_ales[!grepl("Lager", bb_ales$Style, ignore.case=TRUE),]
+
+bb_ales$IsIPA <- grepl("IPA", bb_ales$Style, ignore.case=TRUE)
+
+k_index = sample(seq(1:nrow(bb_ales)),round(0.7*nrow(bb_ales)))
+
+bb_train = bb_ales[k_index,]
+bb_test = bb_ales[-k_index,]
+# k = pick_k(bb_train, bb_test, c("IBU", "ABV"), "IsIPA", nrow(bb_test)) # __7__
+cl <- bb_train[,"IsIPA"]
+truth <- as.factor(bb_test$IsIPA)
+
+pred = knn(bb_train[,c("IBU", "ABV")], bb_test[,c("IBU", "ABV")], cl, k)
+table(predicted = pred, true = truth)
+
+knn(bb_train[,c("IBU", "ABV")], bb_test[,c("IBU", "ABV")], cl, k, prob=TRUE)
+
+bb_ales$IsIPA[bb_ales$IsIPA == TRUE] <- "IPA" 
+bb_ales$IsIPA[bb_ales$IsIPA == FALSE] <- "Not an IPA" 
+
+ggplot(bb_ales, aes(x = IBU, y = ABV * 100.0, color = IsIPA)) +
+  geom_point(position = "jitter", alpha = 0.7) +
+  labs(title = "IPA prediction by ABV and IBU", color = "", y = "ABV (%)") +
+  theme_wsj()   +
+  theme(axis.title=element_text(size=12)) +
+  xlim(0, 150) + ylim(2,10)
+
+confusionMatrix(pred, truth)
 
 
+# 9.
+# Any correlation between ounces and abv?
 
+ggplot(beer_brew_whole, aes(y=ABV * 100.0, x=as.factor(Ounces))) +
+  geom_point(position = "jitter", alpha=0.7)
 
+# Any correlation between high abv states and marijuana legalization? "Party States" 
+mj_dat$State <- state.abb[match(mj_dat$State, state.name)]
+mj_dat$State[is.na(mj_dat$State)] <- "DC"
+
+mj_beers <- beer_brew_whole
+mj_beers <- reduce(list(mj_beers, mj_dat), full_join, by='State')
+names(mj_beers)[names(mj_beers) == 'Status'] <- 'MarijuanaStatus'
+
+ggplot(mj_beers, aes(x=as.factor(State), y=ABV * 100.0, color=MarijuanaStatus)) +
+  geom_point(position = "jitter", alpha=0.7) # +
+  # theme(axis.text.x = element_x(angle=85))
